372f921e46ddbae665d476759fbc5445e5ecc788
Jeferson Aguiar <jefersonitu2010@hotmail.com>
2019-02-27 19:46:59 -0300

    initial commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(publicPath));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+});
