6a406db52d9317d88c92d304e55099cd03cb09a3
Jeferson Aguiar <jefersonitu2010@hotmail.com>
2019-02-27 21:25:45 -0300

    refactoring generateMessage

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const publicPath = path.join(__dirname, '../public');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-
+const { generateMessage } = require('./utils/message');
 const app = express();
 
 const server = http.createServer(app);
@@ -18,25 +18,14 @@ io.on('connection', socket => {
     console.log('The user has disconnected');
   });
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chat app'
-  });
-
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New user joined',
-    createdAt: new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));
+  
+  socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
 
   socket.on('createMessage', ({ from, text }) => {
     console.log('createMessage from client', { from, text });
     
-    socket.broadcast.emit('newMessage', {
-        from,
-        text,
-        createdAt: new Date().getTime()
-    })
+    socket.broadcast.emit('newMessage', generateMessage(from, text));
   });
 });
 
