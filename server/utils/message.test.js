4b1a25675c241f13e462df49940dcf8ae0796234
Jeferson Aguiar <jefersonitu2010@hotmail.com>
2019-02-27 23:26:48 -0300

    adding location tests

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-const { generateMessage } = require('./message');
+const { generateMessage, generateLocationMessage } = require('./message');
 describe('generateMessage', () => {
 
     it('should generate correct message object', () => {
@@ -9,6 +9,21 @@ describe('generateMessage', () => {
 
         const message = generateMessage(from, text);
         expect(message).toMatchObject({ text, from });
+        expect(typeof(message.createdAt)).toBe('number');
+        expect(message.createdAt).toBeTruthy();
+    });
+});
+
+describe('generateLocationMessage', () => {
+
+    it('should generate correct message object', () => {
+
+        const latitude = 1010101;
+        const longitude = 12301230.
+        const from = 'From Test';
+
+        const message = generateLocationMessage(from, latitude, longitude);
+        expect(message).toMatchObject({ from, url: `https://www.google.com/maps?q=${latitude},${longitude}`  });
         expect(typeof(message.createdAt)).toBe('number');
         expect(message.createdAt).toBeTruthy();
     });
